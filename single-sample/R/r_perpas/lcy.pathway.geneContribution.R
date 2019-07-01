4f219b2f8dffa861a281dae928e0db5fe879f270
Chengshu21 <1981893234@qq.com>
2019-07-01 20:24:56 +0800

    Delete lcy.pathway.geneContribution.R

--- single-sample/R/r_perpas/lcy.pathway.geneContribution.R
+++
@@ -1,9 +0,0 @@
-lcy.pathway.geneContribution <- function(paths,plot=FALSE){
-    paths   <- unlist(paths)
-    table   <- table(unlist(paths))
-    table   <- table/length(paths)
-    if(plot){
-        barplot(table,col='blue')
-    }
-    return(table)
-}
