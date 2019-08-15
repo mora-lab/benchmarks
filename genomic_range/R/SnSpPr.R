b12991c0be888e9ed284b3a63d864b73ccfc9bfd
Shaurya Jauhari <ishauryajauhari@gmail.com>
2019-08-15 13:05:15 +0800

    Revise loop structure

--- genomic_range/R/SnSpPr.R
+++ genomic_range/R/SnSpPr.R
@@ -9,7 +9,7 @@ SnSpPr <- function()
   
   
   ## Classical Sensitivity, Specificity, Precision
-
+  
   for ( d in 1:length(disease_pools))
   {
     
@@ -21,41 +21,41 @@ SnSpPr <- function()
     false_negatives <- list()
     count1 <- 1
     count2 <- 1
-  
-    ## Iterating through the entire length of tool results and id-pool gives the same impression as usign "intersect" and "setdiff" set-operations, thereby achieving the common objective; just for flavor here.
-  
-    for (i in 1:nrow(eval(parse(text=(paste0(paste0(tools_results[tool],"$"), ChIPSeqSamples[sam]))))[1]))
+    
+    ## Iterating through the entire length of tool results and id-pool gives the same impression as usign "intersect" and 
+    ## "setdiff" set-operations, thereby achieving the common objective; just for flavor here.
+    
+    for (i in 1:nrow(eval(parse(text=(paste0(paste0(tools_results[tool],"$"), ChIPSeqSamples[sam]))))))
+    {
+      for (j in eval(parse(text=disease_pools[[d]])))
       {
-        for (l in 1:nrow(eval(parse(text=(paste0(paste0(tools_results[tool],"$"), ChIPSeqSamples[sam]))))[2]))
-          {
-            for (j in 1:length(eval(parse(text=disease_pools[d]))))
-              {
-                if (i == j && l <= 0.05)
-                  {
-                    true_positives[[count1]]<- j
-                    count1 <- count1 + 1
-                  }
-                if (i == j && l > 0.05)
-                  {
-                    false_negatives[[count2]]<- j
-                    count2 <- count2 + 1
-                  }
-            }
+        if (eval(parse(text=(paste0(paste0(tools_results[tool],"$"), ChIPSeqSamples[sam]))))[[1]][[i]] == j &&
+            eval(parse(text=(paste0(paste0(tools_results[tool],"$"), ChIPSeqSamples[sam]))))[[2]][[i]] <= 0.05)
+        {
+          true_positives[[count1]]<- j
+          count1 <- count1 + 1
         }
-    }
-  
+        if (eval(parse(text=(paste0(paste0(tools_results[tool],"$"), ChIPSeqSamples[sam]))))[[1]][[i]] == j &&
+            eval(parse(text=(paste0(paste0(tools_results[tool],"$"), ChIPSeqSamples[sam]))))[[2]][[i]] > 0.05)
+        {
+          false_negatives[[count2]]<- j
+          count2 <- count2 + 1
+        }
+      }
+    } 
+    
     ## Tool results' subsets on the basis of statistical significance.
     greater_than_0.05 <- eval(parse(text=(paste0(paste0(tools_results[tool],"$"), ChIPSeqSamples[sam]))))[which(eval(parse(text=(paste0(paste0(tools_results[tool],"$"), ChIPSeqSamples[sam]))))[2] > 0.05),]
     less_than_0.05 <- eval(parse(text=(paste0(paste0(tools_results[tool],"$"), ChIPSeqSamples[sam]))))[which(eval(parse(text=(paste0(paste0(tools_results[tool],"$"), ChIPSeqSamples[sam]))))[2] <= 0.05),]
-    true_negatives_ids <- setdiff(greater_than_0.05[[1]], j) ## All ids that are there in the tool result with p > 0.05 and absent in the disease pool.
-  
-    false_positives1_ids <- intersect(j,greater_than_0.05[1])
-    false_positives2_ids <- setdiff(less_than_0.05[1],j)
-  
+    true_negatives_ids <- setdiff(greater_than_0.05$Geneset.ID, eval(parse(text=disease_pools[d]))) ## All ids that are there in the tool result with p > 0.05 and absent in the disease pool.
+    
+    false_positives1_ids <- intersect(eval(parse(text=disease_pools[d])),greater_than_0.05$Geneset.ID)
+    false_positives2_ids <- setdiff(less_than_0.05$Geneset.ID,eval(parse(text=disease_pools[d])))
+    
     false_positives <- c(false_positives1_ids,false_positives2_ids)
-  
+    
     ## Results
-    cat("\nThe following are the metrics for", as.character(disease_pools[d]),"\n")
+    
     cat("True positives for", as.character(disease_pools[d]), "is",length(true_positives),"\n")
     cat("\nTrue negatives for", as.character(disease_pools[d]), "is",length(true_negatives_ids),"\n")
     cat("\nFalse negatives for", as.character(disease_pools[d]), "is",length(false_negatives),"\n")
@@ -64,9 +64,7 @@ SnSpPr <- function()
     cat("\nPrecision(in percentage) for", as.character(disease_pools[d]), "is", precision*100,"\n")
     sensitivity <- length(true_positives)/(length(true_positives)+length(false_negatives))
     cat("\nSensitivity(in percentage) for", as.character(disease_pools[d]), "is", sensitivity*100,"\n")
-    cat("\nSurrogate Sensitivity(in percentage) for", as.character(disease_pools[d]), "is", for_surrogate_sensitivity*100,"\n")
     specificity <- length(true_negatives_ids)/(length(true_negatives_ids)+length(false_positives))
     cat("\nSpecificity(in percentage) for", as.character(disease_pools[d]), "is", specificity*100,"\n")
-  
   } 
 }
