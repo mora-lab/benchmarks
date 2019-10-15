a8ff2d2a0699ed7b1e3ac7803470a44808823dc6
Shaurya Jauhari <ishauryajauhari@gmail.com>
2019-10-15 14:53:47 +0800

    Revise code for making ggplots

--- genomic_range/R/Plotting_Comparison_Metrics.R
+++ genomic_range/R/Plotting_Comparison_Metrics.R
@@ -21,9 +21,26 @@ Plotting_Comparison_Metrics <- function(){
     
     prioritization_score_table <- read.table(Pr, sep = "\t", header = TRUE, quote = "")
     #jpeg(file="Prioritization.jpeg")
-    prioritization_score_table <- prioritization_score_table[,c(5,10,15,20)]
-    boxplot(prioritization_score_table, ylab = "Prioritization", xlab = "Enrichment Method",col = c("salmon","tan","khaki","lavender"), boxwex = 0.1, ylim = c(0,100), names = c("Chipenrich", "Broadenrich","Seq2pathway", "Enrichr"))
-    abline(prioritization_score_table , h=50, col="red")
+    prioritization_score_table <- prioritization_score_table[,c(5,10,15,20,25)]
+    colnames(prioritization_score_table) <- c("Chipenrich", "Broadenrich", "Seq2pathway", "Enrichr", "GREAT")
+    prioritization_score_table$Samples <- ChIPSeqSamples
+    
+    ## For ggplot2 boxplot
+    library(ggplot2)
+    prioritization_score_table_gather <- gather(prioritization_score_table, Tool, Prioritization_Median_Value, -Samples)
+    ggplot(data = prioritization_score_table_gather,
+           mapping = aes(Tool, Prioritization_Median_Value, fill=Tool, na.rm = FALSE)) +
+      geom_boxplot(varwidth = TRUE) +
+      labs(x= "Tool", y= "Prioritization")
+      
+    
+    # boxplot(prioritization_score_table, 
+    #         ylab = "Prioritization", 
+    #         xlab = "Enrichment Method",
+    #         col = c("salmon","tan","khaki","lavender","darkolivegreen1"), 
+    #         boxwex = 0.1, ylim = c(0,100), 
+    #         names = c("Chipenrich", "Broadenrich","Seq2pathway", "Enrichr", "GREAT"))
+    # abline(prioritization_score_table , h=50, col="red")
     #dev.off()
     
     
@@ -31,38 +48,68 @@ Plotting_Comparison_Metrics <- function(){
     
     surrogate_sensitivity_table <- read.table(SSn, sep = "\t", header = TRUE, quote = "")
     #jpeg(file="Surrogate_Sensitivity.jpeg")
-    surrogate_sensitivity_table <- surrogate_sensitivity_table[,c(5,10,15,20)]
-    boxplot(surrogate_sensitivity_table, ylab = "Sensitivity (P-values of Target Pathways)", xlab = "Enrichment Method",col = c("salmon","tan","khaki","lavender"), boxwex = 0.1, ylim = c(0,1), names = c("Chipenrich", "Broadenrich","Seq2pathway","Enrichr"))
-    abline(surrogate_sensitivity_table, h=0.5, col="red")
-    #dev.off()
+    surrogate_sensitivity_table <- surrogate_sensitivity_table[,c(5,10,15,20,25)]
+    colnames(surrogate_sensitivity_table) <- c("Chipenrich", "Broadenrich", "Seq2pathway", "Enrichr", "GREAT")
+    surrogate_sensitivity_table$Samples <- ChIPSeqSamples
+    
+    ## For ggplot2 boxplot
+   
+    surrogate_sensitivity_table_gather <- gather(surrogate_sensitivity_table, Tool, Prioritization_Median_Value, -Samples)
+    ggplot(data = prioritization_score_table_gather,
+           mapping = aes(Tool, Prioritization_Median_Value, fill=Tool, na.rm = FALSE)) +
+      geom_boxplot(varwidth = TRUE) +
+      labs(x= "Tool", y= "Prioritization")
     
     
     ## Sensitivity Plot | Import the score table from file.
     
     sensitivity_table <- read.table(Sn, sep = "\t", header = TRUE, quote = "")
     #jpeg(file="Sensitivity.jpeg")
-    sensitivity_table <- sensitivity_table[,c(5,10,15,20)]
-    boxplot(sensitivity_table, ylab = "Sensitivity", xlab = "Enrichment Method",col = c("salmon","tan","khaki","lavender"), boxwex = 0.1, ylim = c(0,100), names = c("Chipenrich","Broadenrich","Seq2pathway","Enrichr"))
-    abline(sensitivity_table, h=50, col="red")
-    #dev.off()
+    sensitivity_table <- sensitivity_table[,c(5,10,15,20,25)]
+    colnames(sensitivity_table) <- c("Chipenrich", "Broadenrich", "Seq2pathway", "Enrichr", "GREAT")
+    sensitivity_table$Samples <- ChIPSeqSamples
+   
+     ## For ggplot2 boxplot
+    
+    sensitivity_table_gather <- gather(sensitivity_table, Tool, Sensitivity_Median_Value, -Samples)
+    ggplot(data = sensitivity_table_gather,
+           mapping = aes(Tool, Sensitivity_Median_Value, fill=Tool, na.rm = FALSE)) +
+      geom_boxplot(varwidth = TRUE) +
+      labs(x= "Tool", y= "Sensitivity")
     
     
     ## Specificity Plot | Import the score table from file.
     
     specificity_table <- read.table(Sp, sep = "\t", header = TRUE, quote = "")
     #jpeg(file="Specificity.jpeg")
-    specificity_table <- specificity_table[,c(5,10,15,20)]
-    boxplot(specificity_table, ylab = "Specificity", xlab = "Enrichment Method",col = c("salmon","tan","khaki","lavender"), boxwex = 0.1, ylim = c(0,100), names = c("Chipenrich","Broadenrich","Seq2pathway","Enrichr"))
-    abline(specificity_table, h=50, col="red")
-    #dev.off()
+    specificity_table <- specificity_table[,c(5,10,15,20,25)]
+    colnames(specificity_table) <- c("Chipenrich", "Broadenrich", "Seq2pathway", "Enrichr", "GREAT")
+    specificity_table$Samples <- ChIPSeqSamples
+    
+    ## For ggplot2 boxplot
+    
+    specificity_table_gather <- gather(specificity_table, Tool, Specificity_Median_Value, -Samples)
+    ggplot(data = specificity_table_gather,
+           mapping = aes(Tool, Specificity_Median_Value, fill=Tool, na.rm = FALSE)) +
+      geom_boxplot(varwidth = TRUE) +
+      labs(x= "Tool", y= "Specificity")
+    
     
     
     ## Precision Plot | Import the score table from file.
     
     precision_table <- read.table(Pn, sep = "\t", header = TRUE, quote = "")
     #jpeg(file="Precision.jpeg")
-    precision_table <- precision_table[,c(5,10,15,20)]
-    boxplot(precision_table, ylab = "Precision", xlab = "Enrichment Method",col = c("salmon","tan","khaki","lavender"), boxwex = 0.1, ylim = c(0,3), names = c("Chipenrich","Broadenrich","Seq2pathway","Enrichr"))
-    abline(precision_table, h=0.5, col="red")
-    #dev.off()
+    precision_table <- precision_table[,c(5,10,15,20,25)]
+    colnames(precision_table) <- c("Chipenrich", "Broadenrich", "Seq2pathway", "Enrichr", "GREAT")
+    precision_table$Samples <- ChIPSeqSamples
+    
+    ## For ggplot2 boxplot
+    
+    precision_table_gather <- gather(precision_table, Tool, Precision_Median_Value, -Samples)
+    ggplot(data = precision_table_gather,
+           mapping = aes(Tool, Precision_Median_Value, fill=Tool, na.rm = FALSE)) +
+      geom_boxplot(varwidth = TRUE) +
+      labs(x= "Tool", y= "Precision")
+    
   })}
