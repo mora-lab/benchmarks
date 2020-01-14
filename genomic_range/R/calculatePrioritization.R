683110f2d2c109188b7d6ab43a915eb5db1289bf
Shaurya Jauhari <shauryajauhari@gzhmu.edu.cn>
2020-01-14 14:36:30 +0800

    Improvise functions

--- genomic_range/R/calculatePrioritization.R
+++ genomic_range/R/calculatePrioritization.R
@@ -6,23 +6,23 @@ calculatePrioritization <- function(tool){
   
   for (sam in 1:length(ChIPSeqSamples))
   {
-    for (dis in 1:length(disease_pools))
+    for (dis in 1:length(diseasePools))
     {
       tryCatch({
       ranks <- list()
       x <- 1
-      for (i in 1:length(eval(parse(text=(paste0(paste0(tools_results[3],"$"), ChIPSeqSamples[sam]))))[[1]]))
+      for (i in 1:length(eval(parse(text=(paste0(paste0(toolsResults[tool],"$"), ChIPSeqSamples[sam]))))[[1]]))
         {
-          for (j in (eval(parse(text=disease_pools[[dis]]))))
+          for (j in (eval(parse(text=diseasePools[[dis]]))))
           {
-            if((eval(parse(text=(paste0(paste0(tools_results[3],"$"), ChIPSeqSamples[sam]))))[[1]])[[i]] == j)
+            if((eval(parse(text=(paste0(paste0(toolsResults[tool],"$"), ChIPSeqSamples[sam]))))[[1]])[[i]] == j)
             {
               ranks[[x]]<- i
               x <- x+1
             }
           }
         }
-        findPrioritization <- as.double((ranks[[1]]/nrow((eval(parse(text=(paste0(paste0(tools_results[3],"$"), 
+        findPrioritization <- as.double((ranks[[1]]/nrow((eval(parse(text=(paste0(paste0(toolsResults[tool],"$"), 
                                                                                   ChIPSeqSamples[sam]))))[1]))*100))
         forPrioritization[[sam]][dis] <- findPrioritization
       }, error = function(e){}
@@ -33,21 +33,8 @@ calculatePrioritization <- function(tool){
   ## lists(results) may have different length components. To ensure fullness of the dataframe we shall calculate the maximum length amongst
   ## all (that should technically be the number of diseases), and structure the shorter lists to fit the dimensions of the dataframe by 
   ## explicit zeros. 
-tempList <- list()
 
-for(ind in 1: length(forPrioritization)){tempList <- append(tempList, length(forPrioritization[[ind]]))}
-maxNum <- max(unlist(tempList))
-forPrioritization <- lapply(forPrioritization, function(x) x[1:maxNum])
-
-
-forPrioritization <- as.data.frame(forPrioritization) # transform to a dataframe
-forPrioritization <- as.data.frame(t(forPrioritization)) # transpose the data frame as the output from the function is a list
-row.names(forPrioritization) <- NULL
-colnames(forPrioritization) <- diseasePools
-forPrioritization$Median <- apply(forPrioritization, 1, median) # median value shall be the basis of plotting the results.
-forPrioritization$Samples <- ChIPSeqSamples # add key attribute of sample names. this may be helpful for the purpose of joining dataframes.
-forPrioritization[is.na(forPrioritization)] <- 0 ## replacing NAs with zero for lists shorther than the maximum length.
-return(forPrioritization)
+return(listToFrame(forPrioritization))
 }
 
 ## (For GREAT) Note that in the resultant table, the zero entries reflect that there are no matches between the disease terms and the tool 
